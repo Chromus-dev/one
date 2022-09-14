6e4fbffc0422e8d0a258605ef6d207424aac5b8a
Chromus <xlchannel6@gmail.com>
2022-09-14 21:06:48 +0000

    chicken butt

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let x: number = 5
+
+let y: number = 5
+
+let z: number = 5
+
+console.log(x + y + z)
+
+let joke = {
+    setup: 'guess what',
+    punchLine: 'chicken butt'
+}
+
+console.log(joke.setup)
+console.log(joke.punchLine)
